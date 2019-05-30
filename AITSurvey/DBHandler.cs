303bb1a2c12e70f89ec36ebd0c1ee49f2a64b153
Saoud Nazir <mirzasaoud60@gmail.com>
2019-05-30 20:02:51 +1000

    Inserting Answers in DB

---
+++ AITSurvey/DBHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AITSurvey
+{
+
+
+    public class DBHandler
+    {
+        private static DatabaseTableAdapters.answersTableAdapter ata = new DatabaseTableAdapters.answersTableAdapter();
+        private static Database.answersDataTable adt = new Database.answersDataTable();
+        public static void InsertAnswer(int qID, string answer)
+        {
+            try
+            {
+                int count = ata.InsertAnswer(qID, answer);
+                if (count > 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("Row Added.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+
+        public static void InsertAnswer(int qID, string answer, int sesID)
+        {
+            try
+            {
+                int count = ata.InsertAnswerBySesID(sesID, qID, answer);
+                if (count > 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("Row Added.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+
+        public static void InsertAnswer(int qID, string answer, int sesID, int rID)
+        {
+            try
+            {
+                int count = ata.InsertAnswerBySesAndResID(sesID, rID, qID, answer);
+                if (count > 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("Row Added.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+    }
+}
